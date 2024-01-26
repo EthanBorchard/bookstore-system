d6a82d38c19e8356e13989f5b2d5342a6142a925
eborchard <eborchard@uiowa.edu>
2024-01-26 13:09:44 -0600

    Implement Dashboard details API

---
+++ src/main/java/com/inn/cafe/serviceImpl/DashboardServiceImpl.java
@@ -0,0 +1,32 @@
+package com.inn.cafe.serviceImpl;
+
+import com.inn.cafe.dao.BookDao;
+import com.inn.cafe.dao.CategoryDao;
+import com.inn.cafe.service.DashboardService;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.http.HttpStatus;
+import org.springframework.http.ResponseEntity;
+import org.springframework.stereotype.Service;
+
+import java.util.HashMap;
+import java.util.Map;
+
+@Service
+public class DashboardServiceImpl implements DashboardService {
+
+    @Autowired
+    CategoryDao categoryDao;
+
+    @Autowired
+    BookDao bookDao;
+
+    @Override
+    public ResponseEntity<Map<String, Object>> getCount() {
+        Map<String, Object> map = new HashMap<>();
+        map.put("category", categoryDao.count());
+        map.put("book", bookDao.count());
+
+        return new ResponseEntity<>(map, HttpStatus.OK);
+    }
+
+}
