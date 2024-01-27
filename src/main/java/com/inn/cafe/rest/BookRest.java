36e4bf8bf9f8ea376eca32ced55c9f6c43fcee45
eborchard <eborchard@uiowa.edu>
2024-01-27 10:39:49 -0600

    Add @CrossOrigin annotation to rest controllers.

--- src/main/java/com/inn/cafe/rest/BookRest.java
+++ src/main/java/com/inn/cafe/rest/BookRest.java
@@ -7,6 +7,7 @@ import org.springframework.web.bind.annotation.*;
 import java.util.List;
 import java.util.Map;
 
+@CrossOrigin(origins = "http://localhost:4200/", maxAge = 3600)
 @RequestMapping(path = "/books")
 public interface BookRest {
 
--- src/main/java/com/inn/cafe/rest/DashboardRest.java
+++ src/main/java/com/inn/cafe/rest/DashboardRest.java
@@ -1,11 +1,13 @@
 package com.inn.cafe.rest;
 
 import org.springframework.http.ResponseEntity;
+import org.springframework.web.bind.annotation.CrossOrigin;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.RequestMapping;
 
 import java.util.Map;
 
+@CrossOrigin(origins = "http://localhost:4200/", maxAge = 3600)
 @RequestMapping(path = "/dashboard")
 public interface DashboardRest {
 
